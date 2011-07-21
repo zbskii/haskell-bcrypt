36dc76773c88c7ac26613d12275a948e475e5eeb
zbskii <brett@rdnzl.net>
2011-07-21 15:53:26 -0700

    Better docs, salt deconstructor, test for space leaks

--- examples/Main.hs
+++ examples/Main.hs
@@ -4,11 +4,12 @@ import Data.Digest.BCrypt
 import Control.Monad.Error
 import OpenSSL.Random
 import Data.ByteString ( unpack )
+import Data.ByteString.Char8 ( pack )
 import qualified Data.ByteString.Char8 as B
 
 
-main :: IO ()
-main = do
+example :: IO ()
+example = do
     seed    <- randBytes 16
     badSeed <- randBytes 10
     let salt = genSalt 10 seed
@@ -19,7 +20,16 @@ main = do
     B.putStrLn hashed
     print badSalt
     B.putStrLn hashedBad
-    return ()
+
+main :: IO ()
+main =
+     forM_ [1..100000] $ \n -> do
+                seed <- randBytes 16
+                print $ leakTest' n seed
+
+leakTest' :: Int -> B.ByteString -> B.ByteString
+leakTest' n seed = do
+    maybeHash (pack $ show n) $ genSalt 4 seed
 
 maybeHash :: B.ByteString -> Either String BSalt -> B.ByteString
 maybeHash val salt = case salt of
